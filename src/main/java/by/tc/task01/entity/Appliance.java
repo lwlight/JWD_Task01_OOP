a191b43126b3a254deca70bccebc74de2e3c1759
Artsiom Kniha <lwlight17@gmail.com>
2017-10-13 13:09:10 +0300

    Clone() deleted)

--- src/main/java/by/tc/task01/entity/Appliance.java
+++ src/main/java/by/tc/task01/entity/Appliance.java
@@ -4,9 +4,4 @@ import java.io.Serializable;
 
 public abstract class Appliance implements Serializable{
 	// you may add your own code here
-
-    @Override
-    public Appliance clone() throws CloneNotSupportedException {
-        return (Appliance)super.clone();
-    }
 }
